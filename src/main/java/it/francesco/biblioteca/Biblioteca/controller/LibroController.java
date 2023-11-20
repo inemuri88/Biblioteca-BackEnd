91e2c2a58ae6d21ad3ba766e3f3b6cc5d7d84ead
Francesc Canzaniello <canzaniello.francesco1988@gmail.com>
2023-11-20 13:45:45 +0100

    fix inserimento e getAll libri

--- src/main/java/it/francesco/biblioteca/Biblioteca/controller/LibroController.java
+++ src/main/java/it/francesco/biblioteca/Biblioteca/controller/LibroController.java
@@ -3,6 +3,8 @@ package it.francesco.biblioteca.Biblioteca.controller;
 import it.francesco.biblioteca.Biblioteca.dto.LibroDTO;
 import it.francesco.biblioteca.Biblioteca.service.LibroService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,12 +20,18 @@ public class LibroController {
     Inserimento libro nel db
      */
     @PostMapping("/registrazioneLibro")
-    public void registrazioneLibro(@RequestBody LibroDTO libroDTO){
-        libroService.inserimentoLibro(libroDTO);
+    public ResponseEntity<String> registrazioneLibro(@RequestBody LibroDTO libroDTO){
+        try {
+            libroService.inserimentoLibro(libroDTO);
+            return ResponseEntity.status(HttpStatus.CREATED).body("Libro registrato con successo");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Errore durante la registrazione del libro");
+        }
+
     }
 
     @GetMapping("/getAll")
     public List<LibroDTO> getAll(){
-        return null;
+        return libroService.getAll();
     }
 }
